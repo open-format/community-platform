225b1079f9d5855e38e59e210513d452e8816dc7
andy <andy@openformat.tech>
2025-01-03 11:06:50 +0000

    Remove unused community activity pages

--- src/app/(authenticated)/communities/[slug]/activity/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function Activity({ params }: { params: Promise<{ slug: string }> }) {
-  const slug = (await params).slug;
-  return <div>Authenticated Activity - {slug}</div>;
-}
