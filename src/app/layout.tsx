c086231cfbe5b9b121fb76d93e6806ac1de233d9
izayah jordan <2na@izayahs-MacBook-Pro.local>
2025-01-31 14:45:09 +0000

    updated src/app/layout to use translation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { useTranslations } from 'next-intl';
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -19,8 +20,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const t = useTranslations('Index');
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>{t('title')}</title>
+        <meta name="description" content={t('description')} />
+      </head>
       <body className={`${manrope.variable} antialiased`}>
         <Providers>
           <main>{children}</main>
