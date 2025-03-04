d46fd391582d49ab453a6126bd51a3bf8accb97c
Yunier J. Sosa Vázquez <yuniers@gmail.com>
2025-03-04 14:00:09 -0500

    feat: Add API Key Generation to Community Platform
    - Fix user profile dropdown not disappears after click on it

--- src/components/profile-header.tsx
+++ src/components/profile-header.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Avatar } from "@/components/ui/avatar";
-import { addressSplitter, getAddress } from "@/lib/utils";
-import { usePrivy } from "@privy-io/react-auth";
-import { CopyIcon, ExternalLink, KeySquare, LogOut } from "lucide-react";
-import { useTranslations } from "next-intl";
-import { useState } from "react";
-import { toast } from "sonner";
-import { Button } from "./ui/button";
+import {Avatar} from "@/components/ui/avatar";
+import {addressSplitter, getAddress} from "@/lib/utils";
+import {usePrivy} from "@privy-io/react-auth";
+import {CopyIcon, ExternalLink, KeySquare, LogOut} from "lucide-react";
+import {useTranslations} from "next-intl";
+import {useState} from "react";
+import {toast} from "sonner";
+import {Button} from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,17 +15,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Skeleton } from "./ui/skeleton";
+import {Skeleton} from "./ui/skeleton";
 
 export default function Profile({
-  logoutAction,
-  showNewApiKeyDialogAction,
-}: {
+                                  logoutAction,
+                                  showNewApiKeyDialogAction,
+                                }: {
   logoutAction: () => void;
   showNewApiKeyDialogAction: () => void;
 }) {
-  const { user, ready, exportWallet, authenticated, login } = usePrivy();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const {user, ready, exportWallet, authenticated, login} = usePrivy();
   const t = useTranslations("profile");
   const address = getAddress(user);
 
@@ -36,45 +35,40 @@ export default function Profile({
 
   const exportEnabled = user?.wallet?.walletClientType === "privy";
 
-  const toggle = () => {
-    setIsOpen((t) => !t);
-  };
-
-
   if (!ready) {
-    return <Skeleton className="h-12 w-12 rounded-full" />;
+    return <Skeleton className="h-12 w-12 rounded-full"/>;
   }
 
   return authenticated ? (
-    <DropdownMenu open={isOpen}>
-      <DropdownMenuTrigger onClick={toggle}>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
         {ready ? (
-          <Avatar seed={address || ""} />
+          <Avatar seed={address || ""}/>
         ) : (
-          <Skeleton className="h-12 w-12 rounded-full" />
+          <Skeleton className="h-12 w-12 rounded-full"/>
         )}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem onClick={copyAddress} className="font-bold">
           <span>{addressSplitter(address || "")}</span>
-          <CopyIcon className="ml-auto" />
+          <CopyIcon className="ml-auto"/>
         </DropdownMenuItem>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator/>
         {exportEnabled && (
           <DropdownMenuItem onClick={exportWallet} className="font-bold">
             <span>{t("exportWallet")}</span>
-            <ExternalLink className="ml-auto" />
+            <ExternalLink className="ml-auto"/>
           </DropdownMenuItem>
         )}
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator/>
         <DropdownMenuItem onClick={showNewApiKeyDialogAction} className="font-bold">
           <span>{t("apiKey.apiKey")}</span>
-          <KeySquare className="ml-auto" />
+          <KeySquare className="ml-auto"/>
         </DropdownMenuItem>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator/>
         <DropdownMenuItem onClick={logoutAction} className="font-bold">
           <span>{t("logout")}</span>
-          <LogOut className="ml-auto" />
+          <LogOut className="ml-auto"/>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
