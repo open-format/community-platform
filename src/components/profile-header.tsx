fab8633ae7faf60d62afee5f80aa51b490979d07
Yunier J. Sosa Vázquez <yuniers@gmail.com>
2025-03-13 15:04:59 -0400

    feat: Hide create api key item to not community owners
    - Add function to hide create api key item to not community owners on community details page, only community owners can see it
    - Add cursor copy class

--- src/components/profile-header.tsx
+++ src/components/profile-header.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import {useEffect, useState} from "react";
+import {useParams} from "next/navigation";
 import {Avatar} from "@/components/ui/avatar";
 import {addressSplitter, getAddress} from "@/lib/utils";
 import {usePrivy} from "@privy-io/react-auth";
@@ -16,7 +18,7 @@ import {
 } from "./ui/dropdown-menu";
 import {Skeleton} from "./ui/skeleton";
 import {useApiKey} from "@/hooks/useApikey";
-import {useEffect} from "react";
+import {fetchAllCommunities} from "@/lib/openformat";
 
 export default function Profile({
                                   logoutAction,
@@ -27,6 +29,8 @@ export default function Profile({
   const t = useTranslations("profile");
   const address = getAddress(user);
   const {generateNewApiKey, apiKey, copyApiKeyToClipboard} = useApiKey();
+  const [showCreateApiKey, setShowCreateApiKey] = useState<boolean>(true);
+  const params = useParams();
 
   function copyAddress() {
     navigator.clipboard.writeText(address || "");
@@ -39,12 +43,32 @@ export default function Profile({
         duration: Number.POSITIVE_INFINITY,
         dismissible: true,
         action: {
-          label: <CopyIcon className="h-4 w-4"/>,
+          label: <CopyIcon className="h-4 w-4 cursor-copy"/>,
           onClick: () => copyApiKeyToClipboard(),
         },
       });
     }
   }, [apiKey, copyApiKeyToClipboard, t]);
+
+  useEffect(() => {
+    async function loadCommunities() {
+      if (window.location.pathname.indexOf('communities') !== -1 && params?.slug) {
+        const communities = await fetchAllCommunities();
+
+        if (communities?.data.length) {
+          const isOwner = communities?.data.find(community => community?.id === params.slug);
+
+          if (isOwner === undefined) {
+            setShowCreateApiKey(false);
+          }
+        } else {
+          setShowCreateApiKey(false);
+        }
+      }
+    }
+
+    loadCommunities().then();
+  }, [params.slug]);
 
   const exportEnabled = user?.wallet?.walletClientType === "privy";
 
@@ -73,12 +97,16 @@ export default function Profile({
             <ExternalLink className="ml-auto"/>
           </DropdownMenuItem>
         )}
-        <DropdownMenuSeparator/>
-        <DropdownMenuItem className="font-bold" onClick={generateNewApiKey}>
-          <span>{t("apiKey.createApiKey")}</span>
-          <KeySquare className="ml-auto"/>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator/>
+        {exportEnabled && (<DropdownMenuSeparator/>)}
+        {
+          showCreateApiKey && (
+            <DropdownMenuItem className="font-bold" onClick={generateNewApiKey}>
+              <span>{t("apiKey.createApiKey")}</span>
+              <KeySquare className="ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {showCreateApiKey && (<DropdownMenuSeparator/>)}
         <DropdownMenuItem onClick={logoutAction} className="font-bold">
           <span>{t("logout")}</span>
           <LogOut className="ml-auto"/>
