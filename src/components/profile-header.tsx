2139a4f9bac476daf576a33cef20b3eb55737f27
Yunier J. Sosa Vázquez <yuniers@gmail.com>
2025-03-17 22:54:28 -0400

    feat: Only check if communities in the pathname.

--- src/components/profile-header.tsx
+++ src/components/profile-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {useEffect, useState} from "react";
-import {useParams} from "next/navigation";
+import {usePathname} from "next/navigation";
 import {Avatar} from "@/components/ui/avatar";
 import {addressSplitter, getAddress} from "@/lib/utils";
 import {usePrivy} from "@privy-io/react-auth";
@@ -18,19 +18,18 @@ import {
 } from "./ui/dropdown-menu";
 import {Skeleton} from "./ui/skeleton";
 import {useApiKey} from "@/hooks/useApikey";
-import {fetchAllCommunities} from "@/lib/openformat";
 
 export default function Profile({
-                                  logoutAction,
-                                }: {
+  logoutAction,
+}: {
   logoutAction: () => void;
 }) {
   const {user, ready, exportWallet, authenticated, login} = usePrivy();
   const t = useTranslations("profile");
   const address = getAddress(user);
   const {generateNewApiKey, apiKey, copyApiKeyToClipboard} = useApiKey();
-  const [showCreateApiKey, setShowCreateApiKey] = useState<boolean>(true);
-  const params = useParams();
+  const [showCreateApiKey, setShowCreateApiKey] = useState<boolean>(false);
+  const pathname = usePathname();
 
   function copyAddress() {
     navigator.clipboard.writeText(address || "");
@@ -51,24 +50,10 @@ export default function Profile({
   }, [apiKey, copyApiKeyToClipboard, t]);
 
   useEffect(() => {
-    async function loadCommunities() {
-      if (window.location.pathname.indexOf('communities') !== -1 && params?.slug) {
-        const communities = await fetchAllCommunities();
-
-        if (communities?.data.length) {
-          const isOwner = communities?.data.find(community => community?.id === params.slug);
-
-          if (isOwner === undefined) {
-            setShowCreateApiKey(false);
-          }
-        } else {
-          setShowCreateApiKey(false);
-        }
-      }
+    if (pathname.includes("communities")) {
+      setShowCreateApiKey(true);
     }
-
-    loadCommunities().then();
-  }, [params.slug]);
+  }, [pathname]);
 
   const exportEnabled = user?.wallet?.walletClientType === "privy";
 
