862f3222f16b9a4c6f4ea1294a0f24441c23816d
andy <andy@openformat.tech>
2025-04-24 12:15:59 +0100

    refactor: update matchain subgraph URL

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -34,8 +34,8 @@ export type Chain = {
     maxPriorityFeePerGas: bigint;
   };
   testnet: boolean;
-  subgraph_max_first: number,
-  subgraph_max_skip: number,
+  subgraph_max_first: number;
+  subgraph_max_skip: number;
 };
 
 export enum ChainName {
@@ -56,7 +56,7 @@ export const chains: Record<ChainName, Chain> = {
     apiChainName: "arbitrum-sepolia",
     testnet: true,
     subgraph_max_first: 1000,
-    subgraph_max_skip: 5000,  
+    subgraph_max_skip: 5000,
   },
   aurora: {
     APP_FACTORY_ADDRESS: "0x2eBF7f4572c218217ca01CE2883E3EfF93626a8E",
@@ -66,18 +66,19 @@ export const chains: Record<ChainName, Chain> = {
     ...aurora,
     testnet: false,
     subgraph_max_first: 1000,
-    subgraph_max_skip: 5000,  
+    subgraph_max_skip: 5000,
   },
   turbo: {
     APP_FACTORY_ADDRESS: "0x7e405FbA4c29B8B05B5ecF97bA664729C34803B8",
     // @TODO: Update to production subgraph when ready
-    SUBGRAPH_URL: "https://openformat-turbo-graph-node-staging.fly.dev/subgraphs/name/open-format-local",
+    SUBGRAPH_URL:
+      "https://openformat-turbo-graph-node-staging.fly.dev/subgraphs/name/open-format-local",
     BLOCK_EXPLORER_URL: "https://explorer.turbo.aurora.dev",
     apiChainName: "turbo",
     ...turboChain,
     testnet: false,
     subgraph_max_first: 1000,
-    subgraph_max_skip: -1,  
+    subgraph_max_skip: -1,
   },
   base: {
     APP_FACTORY_ADDRESS: "0x2d6f1620b263Ce71862Fa95f6fEAbB6A366478cC",
@@ -87,17 +88,18 @@ export const chains: Record<ChainName, Chain> = {
     ...base,
     testnet: false,
     subgraph_max_first: 1000,
-    subgraph_max_skip: 5000,  
+    subgraph_max_skip: 5000,
   },
   matchain: {
     APP_FACTORY_ADDRESS: "0xf1811D1D6D9a718312c3c9466D8c4a2601f973e7",
-    SUBGRAPH_URL: "http://34.45.39.186:8000/subgraphs/id/QmZa1CpWzSWiVTftZGMrFbyboHgW49j56h22vCd5fdmgVU",
+    SUBGRAPH_URL:
+      "https://subgraph.mobula.io/subgraphs/id/QmZa1CpWzSWiVTftZGMrFbyboHgW49j56h22vCd5fdmgVU",
     BLOCK_EXPLORER_URL: "https://matchscan.io/",
     apiChainName: "matchain",
     ...matchain,
     testnet: false,
     subgraph_max_first: 1000,
-    subgraph_max_skip: 5000,  
+    subgraph_max_skip: 5000,
   },
 };
 
