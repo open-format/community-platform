577d73e32c54ac9bed01c79e2455989319808dc3
2na <izayahjordan13@gmail.com>
2025-05-23 11:51:52 +0100

    refactor: Enhance middleware logic for user authentication and route handling
    - Added conditions to bypass middleware for specific Discord callback routes.
    - Simplified pathname checks for user redirection to improve clarity and maintainability.
    - Updated middleware configuration to ensure proper routing for onboarding paths.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,32 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+  const pathname = url.pathname;
+
+  // Bypass middleware for Discord callback and its redirect as privy does not set the cookies in time
+  if (
+    pathname === "/api/discord/callback" ||
+    (pathname === "/onboarding/integrations" && url.searchParams.has("guildId"))
+  ) {
+    return NextResponse.next();
+  }
+
   const user = await getCurrentUser();
 
   // Redirect to /communities if user is logged in and on home page or auth page
-  if (user && (request.nextUrl.pathname === "/" || request.nextUrl.pathname === "/auth")) {
+  if (user && (pathname === "/" || pathname === "/auth")) {
     return NextResponse.redirect(new URL("/communities", request.url));
   }
 
-  // Redirect to auth if no user
-  if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
+  // Redirect to auth if no user and not on auth page
+  if (!user && !pathname.startsWith("/auth")) {
     return NextResponse.redirect(new URL("/auth", request.url));
   }
 
   // For the specific communities check
-  if (request.nextUrl.pathname.startsWith("/communities/")) {
-    const slug = request.nextUrl.pathname.split("/")[2];
+  if (pathname.startsWith("/communities/")) {
+    const slug = pathname.split("/")[2];
     if (!user?.apps?.includes(slug)) {
       return NextResponse.redirect(new URL("/", request.url));
     }
@@ -27,5 +38,11 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/(authenticated)/:path*", "/auth", "/onboarding/:path*", "/onboarding"],
+  matcher: [
+    "/",
+    "/(authenticated)/:path*",
+    "/auth",
+    "/onboarding/:path*",
+    "/onboarding",
+  ],
 };
